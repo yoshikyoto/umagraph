b69b4451bea61e2a2053d71ecd548b3a72f3315e
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-06-30 15:28:50 +0900

    keiba client 一応完成

--- src/infra/keiba/keiba_client.rb
+++ src/infra/keiba/keiba_client.rb
@@ -28,7 +28,7 @@ class KeibaClient
     page = get_params({acc_param: 'race'})
     page.css('.today_mainrace').map{|race_dom|
       hash = parse_race(race_dom)
-      get_race_detail(hash[:path], hash[:name])
+      puts get_race_detail(hash[:path], hash[:name])
     }
   end
 
@@ -40,13 +40,41 @@ class KeibaClient
   end
 
   def get_race_detail(path, race_title = nil)
-    puts 'get_race_detail'
     page = get_path(path)
-    page.css('.bml1').map{|bml_dom|
+    page.css('.bml1').map{|dom|
+      waku = dom.css('td > span').first.content.strip.to_i
+      num = dom.css('.umaban').first.content.strip.to_i
       
+      horse_name_dom = dom.css('.horsename a').first
+      name = horse_name_dom.content.strip
+      url = horse_name_dom.attributes['href'].value.strip
+      
+      sex_age = dom.css('td')[4].content.strip.split("")
+      sex = sex_age[0]
+      age = sex_age[1].to_i
+      
+      burden = dom.css('td')[5].content.strip.to_f
+      jockey_dom = dom.css('td')[6].css('a').first
+      jockey_name = jockey_dom.content.strip
+      jockey_url = jockey_dom.attributes['href'].value.strip
+
+      odds = dom.css('td')[7].content.strip.to_f
+      popularity = dom.css('td')[8].content.strip.to_i
+      {
+        waku: waku,
+        num: num,
+        name: name,
+        url: url,
+        sex: sex,
+        age: age,
+        burden: burden,
+        jockey: {
+          name: jockey_name,
+          url: jockey_url,
+        },
+        odds: odds,
+        popularity: popularity
+      }
     }
   end
 end
-
-client = KeibaClient.new()
-client.get_main_race_list()
